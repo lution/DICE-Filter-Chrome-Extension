591380826a9759dd4aa5d581979354b27102d3b3
Hammer <lution798@gmail.com>
2023-07-11 00:05:23 -0700

    bugfix: after navigating between pages, filter did not work

--- js/content.js
+++ js/content.js
@@ -18,6 +18,7 @@ function doFilter(keywords) {
     const searchResults = searchResultsContainer.querySelectorAll('.search-card');
 
     // Iterate over the search result elements and hide those that don't match the keywords
+    var cnt = 0;
     for (const result of searchResults) {
         const jobTitleElement = result.querySelector('.card-title-link');
         const jobTitle = jobTitleElement.textContent.toLowerCase();
@@ -27,9 +28,39 @@ function doFilter(keywords) {
 
         if (isMatchingKeyword) {
             result.style.display = 'none';
+            cnt++;
         }
     }
+    console.log(`[DEBUG] filtered ${cnt} jobs`);
 }
+
+// Function to observe changes in the DOM
+/*
+function observeDOM() {
+    // Target the search result container parent element
+    const parentElement = document.body;
+
+    // Create a new MutationObserver
+    const observer = new MutationObserver(function(mutations) {
+        for (const mutation of mutations) {
+            for (const node of mutation.addedNodes) {
+                if (node.id === 'searchDisplay-div') {
+                    // If the desired div is added to the DOM, stop observing and apply the filtering logic
+                    //observer.disconnect();
+                    applyFiltering();
+                    return;
+                }
+            }
+        }
+    });
+
+    // Configure and start observing
+    observer.observe(parentElement, {
+        childList: true,
+        subtree: true
+    });
+}
+*/
 
 // Function to observe changes in the DOM
 function observeDOM() {
@@ -44,6 +75,7 @@ function observeDOM() {
                     // If the desired div is added to the DOM, stop observing and apply the filtering logic
                     observer.disconnect();
                     applyFiltering();
+                    observeContentChanges(document.getElementById('searchDisplay-div'));
                     return;
                 }
             }
@@ -52,6 +84,24 @@ function observeDOM() {
 
     // Configure and start observing
     observer.observe(parentElement, {
+        childList: true,
+        subtree: true
+    });
+}
+
+// Function to observe changes in the content of searchDisplay-div
+function observeContentChanges(searchDisplayDiv) {
+    const observer = new MutationObserver(function(mutations) {
+        observer.disconnect();
+        applyFiltering();
+        observer.observe(searchDisplayDiv, {
+            childList: true,
+            subtree: true
+        });
+    });
+
+    // Start observing the searchDisplay-div for content changes
+    observer.observe(searchDisplayDiv, {
         childList: true,
         subtree: true
     });
